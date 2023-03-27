e90313981a54f939beb408b5dc5157bffb8c124d
VictorA97 <63431284+VictorA97@users.noreply.github.com>
2023-03-26 22:34:05 -0300

    Primer commit final react

---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>ElectroTech</h1>
+        <p>el lugar correcto para vos</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/componets/Products/Products.js
@@ -0,0 +1,29 @@
+import { useContext, useState, useEffect } from "react";
+import { dataContext } from "../Context/DataContext";
+import axios from "axios";
+
+import "./Products.css";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const { buyProducts } = useContext(dataContext);
+  const {description} =useContext(dataContext);
+
+  useEffect(() => {
+    axios("data.json").then((res) => setData(res.data));
+  }, []);
+
+  return data.map((product) => {
+    return (
+      <div className='card' key={product.id}>
+        <img src={product.img} alt='img-product-card' />
+        <h3>{product.name}</h3>
+        <h4>{product.price}$</h4>
+        <button onClick={() => buyProducts(product)}>buy</button>
+        <button onClick={() => description(product)}>descripcion</button>
+      </div>
+    );
+  });
+};
+
+export default Products;
